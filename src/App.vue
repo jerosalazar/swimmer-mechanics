06b030f6d297f42ae076139f8d47af620b2e774a
Jeronimo Salazar Salas <jcsalazar@uc.cl>
2020-12-06 12:05:01 -0300

    Implement charts

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <VueFormulate/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueFormulate from './components/VueFormulate.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VueFormulate,
   }
 }
 </script>
---
+++ src/components/VueFormulate.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="row">
+    <div class="column">
+      <h2>Mecánica de la natación</h2>
+      <FormulateForm v-model="formValues" @submit="handleSubmit">
+        <FormulateInput
+          name="swimmer"
+          :options="{
+            first: 'Nadador 1',
+            second: 'Nadador 2',
+            third: 'Nadador 3',
+            fourth: 'Nadador 4'
+            }"
+          value="first"
+          type="radio"
+          label="Selecciona el nadador"
+        />
+        <FormulateInput
+          label="Densidad del medio"
+          type="range"
+          name="densityValue"
+          min="700"
+          max="1240"
+          :value="density"
+          show-value="true"
+        />
+        <FormulateInput
+          label="Ángulo del nadador"
+          type="range"
+          name="swimmerAngle"
+          min="0"
+          max="50"
+          :value="angle"
+          show-value="true"
+        />
+        <FormulateInput type="submit" label="Submit"/>
+      </FormulateForm>
+    </div>
+    <div class="column">
+      <DragChart
+        :density-value="density"
+        :swimmer-angle="angle"
+        :swimmer="swimmer"
+      />
+      <SustentationChart
+        :density-value="density"
+        :swimmer-angle="angle"
+        :swimmer="swimmer"
+      />
+    </div>
+    {{ formValues }}
+  </div>
+</template>
+
+<script>
+import DragChart from './DragChart.vue'
+import SustentationChart from './SustentationChart.vue'
+
+export default {
+  components: {
+    DragChart,
+    SustentationChart,
+  },
+  data() {
+    return {
+      formValues: {},
+      density: 1000,
+      angle: 10,
+      swimmerOptions: {
+        first: {
+          length: 1.7,
+          width: 0.3,
+          top: 0.2,
+        },
+        second: {
+          length: 1.8,
+          width: 0.5,
+          top: 0.4,
+        },
+        third: {
+          length: 1.65,
+          width: 0.25,
+          top: 0.2,
+        },
+        fourth: {
+          length: 1.65,
+          width: 0.4,
+          top: 0.3,
+        },
+      },
+      swimmer: {
+          length: 1.8,
+          width: 0.5,
+          top: 0.4,
+        },
+      };
+  },
+  methods: {
+    handleSubmit() {
+      this.density = parseInt(this.formValues.densityValue);
+      this.angle = parseInt(this.formValues.swimmerAngle);
+      this.swimmer = this.swimmerOptions[this.formValues.swimmer];
+    },
+  },
+  
+}
+</script>
+
+<style>
+.column {
+  float: left;
+  width: 50%;
+  text-align: left;
+}
+
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+</style>
